99dab4bf2dca26c9710aa826432caa4ce3ec7caa
Phil <pcao@risd.edu>
2012-10-02 00:23:52 -0400

    Archives

--- archives.php
+++ archives.php
@@ -6,13 +6,9 @@ Template Name: Archives
 <html>
 
 <head>
-	
-	<title><?php bloginfo("Newspaper"); ?><?php wp_title(); ?></title>
-	<meta charset="<?php bloginfo('charset'); ?>" />
-	<meta name="generator" content="Wordpress <?php bloginfo('version'); ?>" />
-	<meta name="description" content="<?php bloginfo('description'); ?>">
 	<link rel="stylesheet" href="<?php bloginfo("stylesheet_url"); ?>" type="text/css" media="screen" />
-	<link rel="shortcut icon" href="<?php bloginfo('url') ?>/wp-content/themes/newspaper/images/favicon.ico">
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+	<title>Archives</title>
 
 
 
@@ -28,24 +24,17 @@ Template Name: Archives
 
 		<?php the_post(); ?>
 
-
-
 		<!--The Title of the Page Entry-->
-		<div id="ArchiveHeader"><div id="ArchiveEntry"><h5 class="entry-title"><?php the_title(); ?></h5></div><?php get_search_form(); ?></div>
+		<h5 class="entry-title"><?php the_title(); ?></h5>
 
 
 		
+		<?php get_search_form(); ?>
 		
-		<div id="ArchiveBoxes">
+		
 		<ul>
-
-			<?php wp_get_archives('type=monthly&limit=6&show_post_count=0&date_format()
-
-			'); ?>
+			<?php wp_get_archives('type=monthly&limit=6&show_post_count=0'); ?>
 		</ul>
-
-		<h3><a href='*/full-archive'>More...</a></h3>
-		</div>
 		
 
 	</div><!-- #content -->
